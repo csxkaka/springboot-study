2d69279c05b6608c92a84c9e46ec0e0fa01d9b34
csxkaka <970972786@qq,com>
2019-10-18 00:58:40 +0800

    函数式接口

--- springboot-study/src/main/java/com/csxkaka/springbootstudy/config/MyCommandLineRunner2.java
+++ springboot-study/src/main/java/com/csxkaka/springbootstudy/config/MyCommandLineRunner2.java
@@ -3,6 +3,7 @@ package com.csxkaka.springbootstudy.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,17 @@ import java.util.Arrays;
  * 配置第二个项目启动后执行的业务类
  * 查看springboot帮我们自动注册了哪些类
  */
-//@Component
+
+/**
+ * 函数式接口：jdk1.8新特性
+ * CommandLineRunner是一个函数式接口，使用@FunctionalInterface注解
+ * 函数式接口特点：
+ * 有且只有一个抽象方法，
+ * 可以有静态方法(static) 有方法体，
+ * 可以有默认方法(default) 有方法体,
+ * 可以有Object的public方法
+ */
+@Component
 //@Order(2)
 public class MyCommandLineRunner2 implements CommandLineRunner {
 
@@ -27,6 +38,22 @@ public class MyCommandLineRunner2 implements CommandLineRunner {
         System.out.println("Let's inspect the beans provided by Spring Boot!");
         for (String beanName : beanNames) {
             System.out.println(beanName);
-       }
+        }
+    }
+
+    /**
+     * 也可以这样获取beans
+     * @param ctx
+     * @return
+     */
+    //@Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            String[] beanNames =  ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
     }
 }
